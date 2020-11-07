86a8e4e90b3fb5362300cbe1040423ae9f955f50
obarra <barraomar12@gmail.com>
2020-11-07 12:15:07 -0300

    test recover and panic

--- go-115/panic_recover_test.go
+++ go-115/panic_recover_test.go
@@ -5,25 +5,49 @@ import (
 	"testing"
 )
 
+//is a built-in function that stops the ordinary flow of control and begins panicking.
+//The process continues up the stack until all functions in the current goroutine have returned
+//So you can see the stacktrace
 func TestPanic(t *testing.T) {
-	t.SkipNow()
-	doSomething()
+	doSomethingWithOutRecover(1)
 }
 
 func TestRecover(t *testing.T) {
-	doSomethingWithRecover()
+	result := doSomethingWithRecover()
 	fmt.Println("Recovered")
+	if result == "Returned normally from doSomethingDeferRecover." {
+		t.Log("OK")
+	} else {
+		t.Error("Error", result)
+	}
 }
 
-func doSomething() {
-	panic("Panic Test")
+func doSomethingWithOutRecover(counter int) {
+	counterAndDoPanic(counter)
+}
+func counterAndDoPanic(counter int) {
+	if counter > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", counter))
+	}
+	counterAndDoPanic(counter + 1)
 }
 
-func doSomethingWithRecover() {
+func doSomethingWithRecover() string {
+	doSomethingDeferRecover()
+	return "Returned normally from doSomethingDeferRecover."
+}
+
+func doSomethingDeferRecover() {
 	fmt.Printf("Doing something...")
 	defer func() {
-		messageError := recover()
-		fmt.Println(messageError)
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in doSomethingDeferRecover:", r)
+		} else {
+			fmt.Println("Error recovering!!")
+		}
+
 	}()
-	panic("Panic Test with recover")
+	counterAndDoPanic(1)
+	fmt.Println("Returned normally from counterWithPanic. Never happen")
 }
