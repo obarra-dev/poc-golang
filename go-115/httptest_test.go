effe90cec2f6422cd8d1733425ec34b64fcbc3df
obarra <barraomar12@gmail.com>
2020-12-08 23:43:31 -0300

    test http client and server

--- go-115/httptest_test.go
+++ go-115/httptest_test.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-func TestReturnImmediatelyTimeOut(t *testing.T) {
+func TestServerReturnImmediatelyTimeOut(t *testing.T) {
 	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusGatewayTimeout)
 	}))
@@ -37,7 +37,7 @@ func TestReturnImmediatelyTimeOut(t *testing.T) {
 	}
 }
 
-func TestReturnTimeOutWithHandler(t *testing.T) {
+func TestServerReturnTimeOutWithHandler(t *testing.T) {
 	handlerFuncSleep100ms := http.HandlerFunc(handlerWithSleep100ms)
 
 	//time out when  Any value < 100 ms
@@ -64,7 +64,7 @@ func TestReturnTimeOutWithHandler(t *testing.T) {
 	}
 }
 
-func TestReturn200(t *testing.T) {
+func TestServerReturn200(t *testing.T) {
 	handlerFuncSleep100ms := http.HandlerFunc(handlerWithSleep100ms)
 
 	//time out when  Any value < 100 ms
@@ -79,6 +79,35 @@ func TestReturn200(t *testing.T) {
 	}
 
 	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Error("Response error", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Error("Error", resp.StatusCode, resp.Body)
+	}
+}
+
+func TestClientSetTimeOut(t *testing.T) {
+	handlerFuncSleep100ms := http.HandlerFunc(handlerWithSleep100ms)
+
+	//time out when  Any value < 100 ms
+	backend := httptest.NewServer(handlerFuncSleep100ms)
+
+	url := backend.URL
+	fmt.Println(url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Error("Request error", err)
+		return
+	}
+
+	client := http.Client{Timeout: 200 * time.Millisecond}
+
+	resp, err := client.Do(req)
 	if err != nil {
 		t.Error("Response error", err)
 		return
