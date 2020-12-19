d6f74a50fbcf06ebd4c7928c1c179ba4f1a23490
obarra <barraomar12@gmail.com>
2020-12-19 11:02:47 -0300

    adjust test

--- go-115/string_test.go
+++ go-115/string_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 	"testing"
@@ -14,9 +15,7 @@ func TestStringSplit(t *testing.T) {
 
 	segments := strings.Split(myString, "/products")
 
-	if len(segments) == 3 {
-		t.Log("OK")
-	} else {
+	if len(segments) != 3 {
 		t.Error("Error", segments, len(segments))
 	}
 }
@@ -30,20 +29,6 @@ func TestStringStrconv(t *testing.T) {
 		t.Error("Error", number)
 	}
 }
-
-func TestStringBuilder(t *testing.T) {
-	var builder strings.Builder
-	builder.WriteString("First")
-	builder.WriteString("Second")
-	result := builder.String()
-
-	if result == "FirstSecond" {
-		t.Log("OK")
-	} else {
-		t.Error("Error", result)
-	}
-}
-
 func TestIntToString(t *testing.T) {
 	result := strconv.Itoa(404)
 	if result != "404" {
@@ -56,6 +41,76 @@ func TestStringsJoin(t *testing.T) {
 	result := strings.Join(array[:], ",")
 
 	if result != "a,b,c" {
+		t.Error("Error", result)
+	}
+}
+
+func TestStringBuilder(t *testing.T) {
+	var builder strings.Builder
+	builder.WriteString("First")
+	builder.WriteString("Second")
+	result := builder.String()
+
+	if result != "FirstSecond" {
+		t.Error("Error", result)
+	}
+}
+
+func TestRuneJoinWithStringsBuilder(t *testing.T) {
+	array := [3]rune{'a', 'b', 'c'}
+
+	var sb strings.Builder
+
+	for _, char := range array {
+		sb.WriteRune(char)
+	}
+
+	result := sb.String()
+
+	if result != "abc" {
+		t.Error("Error", result)
+	}
+}
+
+func TestStringIterationWithRange(t *testing.T) {
+	namestring := "omar barra"
+
+	var sb strings.Builder
+	for _, v := range namestring {
+
+		sb.WriteRune(v)
+	}
+
+	result := sb.String()
+	if result != "omar barra" {
+		t.Error("Error", result)
+	}
+}
+
+func TestStringIteration(t *testing.T) {
+	namestring := "omar barra"
+
+	var sb strings.Builder
+	for i := 0; i < len(namestring); i++ {
+		sb.WriteByte(namestring[i])
+	}
+
+	result := sb.String()
+	if result != "omar barra" {
+		t.Error("Error", result)
+	}
+}
+
+//string.Builder also implements the io.Writer interface
+func TestStringBuilderFormat(t *testing.T) {
+	var sb strings.Builder
+	for _, v := range [3]int{1, 2, 3} {
+		fmt.Fprintf(&sb, "%d...", v)
+	}
+
+	result := sb.String()
+
+	if result != "1...2...3..." {
 		t.Error("Error", result)
 	}
 }
@@ -73,5 +128,52 @@ func TestStringMultiLine(t *testing.T) {
 
 	if result != expected {
 		t.Error("Error", result, expected)
+	}
+}
+
+func TestAscii(t *testing.T) {
+	result := string('o')
+	//results := string(11)
+
+	if result != "os" {
+		t.Error("Error", result)
+	}
+}
+
+//both byte and rune are essentially integers
+//byte is aliases for unit8
+//rune is aliases for int32
+func TestBtyeVsRune(t *testing.T) {
+	letterO := 'o'
+	var letterM byte = 'm'
+	var letterA rune = 'a'
+	var letterR byte = 114
+	letterB := byte('B')
+
+	resultO := string(letterO)
+	resultM := string(letterM)
+	resultA := string(letterA)
+	resultR := string(letterR)
+	resultB := string(letterB)
+
+	if resultO != "o" || resultM != "m" ||
+		resultA != "a" || resultR != "r" || resultB != "B" {
+		t.Error("Error", resultO, resultM, resultA, resultR, resultB)
+	}
+}
+
+func TestStringToByteSliceAndViceVersa(t *testing.T) {
+	nameString := "omar barra"
+
+	nameSlice := []byte(nameString)
+
+	if len(nameSlice) != 10 {
+		t.Error("Error", nameSlice)
+	}
+
+	other := string(nameSlice)
+
+	if other != nameString {
+		t.Error("Error", other, nameString)
 	}
 }
