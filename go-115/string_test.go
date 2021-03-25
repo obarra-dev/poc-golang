c0408901f16d776875ff93f97cd9d845ee3aef06
obarra-dev <barra_omar@hotmail.com>
2021-03-25 19:43:13 -0300

    test string

--- go-115/string_test.go
+++ go-115/string_test.go
@@ -20,15 +20,28 @@ func TestStringSplit(t *testing.T) {
 	}
 }
 
+//Atoi is equivalent to ParseInt(s, 10, 0)
 func TestStringStrconv(t *testing.T) {
 	number, _ := strconv.Atoi("998")
 
-	if number == 998 {
-		t.Log("OK")
-	} else {
+	if number != 998 {
 		t.Error("Error", number)
 	}
 }
+
+//Use strconv.ParseInt to parse a decimal string (base 10) and check if it fits into an int64.
+func TestStringParseInt(t *testing.T) {
+	number, err := strconv.ParseInt("-128", 10, 8)
+	if number != -128 || err != nil {
+		t.Error("Error", number, err)
+	}
+
+	number, err = strconv.ParseInt("-129", 10, 8)
+	if number != -128 || err.Error() != `strconv.ParseInt: parsing "-129": value out of range` {
+		t.Error("Error", number, err.Error())
+	}
+}
+
 func TestIntToString(t *testing.T) {
 	result := strconv.Itoa(404)
 	if result != "404" {
