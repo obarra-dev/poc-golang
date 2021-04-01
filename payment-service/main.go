56cab248e9022b7a66e66886efb48177c32753e4
obarra-dev <barra_omar@hotmail.com>
2021-04-01 15:35:07 -0300

    use custom handler

--- payment-service/main.go
+++ payment-service/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+
 	websockets "poc-golang/payment-service/webservices/websocket"
 
 	"poc-golang/payment-service/webservices/cbu"
 	"poc-golang/payment-service/webservices/database"
 	"poc-golang/payment-service/webservices/product"
 	"poc-golang/payment-service/webservices/report"
+	"poc-golang/payment-service/webservices/user"
 )
 
 const basePathAPI = "/api"
@@ -20,6 +22,7 @@ func main() {
 
 	cbu.SetupRoutes(basePathAPI)
 	product.SetupRoutes(basePathAPI)
+	user.SetupRoutes(basePathAPI)
 
 	report.SetupRoutes(basePathWEB)
 
---
+++ payment-service/user/user.go
@@ -0,0 +1,129 @@
+package user
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"poc-golang/payment-service/webservices/middleware"
+	"regexp"
+	"sync"
+)
+
+var (
+	listUserRe   = regexp.MustCompile(`^\/api\/users[\/]*$`)
+	getUserRe    = regexp.MustCompile(`^\/api\/users\/(\d+)$`)
+	createUserRe = regexp.MustCompile(`^\/api\/users[\/]*$`)
+)
+
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type datastore struct {
+	m map[string]user
+	*sync.RWMutex
+}
+
+type userHandler struct {
+	store *datastore
+}
+
+func SetupRoutes(apiBasePath string) {
+	userH := &userHandler{
+		store: &datastore{
+			m: map[string]user{
+				"1": {ID: "1", Name: "bob"},
+			},
+			RWMutex: &sync.RWMutex{},
+		},
+	}
+
+	http.Handle(fmt.Sprintf("%s/users", apiBasePath), middleware.Middleware(userH))
+	http.Handle(fmt.Sprintf("%s/users/", apiBasePath), middleware.Middleware(userH))
+}
+
+func (h *userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	switch {
+	case r.Method == http.MethodGet && listUserRe.MatchString(r.URL.Path):
+		h.List(w, r)
+		return
+	case r.Method == http.MethodGet && getUserRe.MatchString(r.URL.Path):
+		h.Get(w, r)
+		return
+	case r.Method == http.MethodPost && createUserRe.MatchString(r.URL.Path):
+		h.Create(w, r)
+		return
+	default:
+		notFound(w, r)
+		return
+	}
+}
+
+func (h *userHandler) List(w http.ResponseWriter, r *http.Request) {
+	h.store.RLock()
+	users := make([]user, 0, len(h.store.m))
+	for _, v := range h.store.m {
+		users = append(users, v)
+	}
+	h.store.RUnlock()
+	jsonBytes, err := json.Marshal(users)
+	if err != nil {
+		internalServerError(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *userHandler) Get(w http.ResponseWriter, r *http.Request) {
+	matches := getUserRe.FindStringSubmatch(r.URL.Path)
+	if len(matches) < 2 {
+		notFound(w, r)
+		return
+	}
+	h.store.RLock()
+	u, ok := h.store.m[matches[1]]
+	h.store.RUnlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("user not found"))
+		return
+	}
+	jsonBytes, err := json.Marshal(u)
+	if err != nil {
+		internalServerError(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *userHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var u user
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		internalServerError(w, r)
+		return
+	}
+	h.store.Lock()
+	h.store.m[u.ID] = u
+	h.store.Unlock()
+	jsonBytes, err := json.Marshal(u)
+	if err != nil {
+		internalServerError(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func internalServerError(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("internal server error"))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("not found"))
+}
