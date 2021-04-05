ded7e0c90a391169673adc0f4eca3c2a699d5298
obarra-dev <barra_omar@hotmail.com>
2021-04-04 22:47:58 -0300

    test switch

--- go-115/switch_test.go
+++ go-115/switch_test.go
@@ -78,3 +78,26 @@ func TestSwitchWithNoConditionFallThrough(t *testing.T) {
 		t.Error("Error", result)
 	}
 }
+
+var src = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+//, break statements can be used to terminate a switch early. Sometimes, though, it's necessary to break out of a surrounding loop
+func TestSwitchWithLabel(t *testing.T) {
+	var result int
+Loop:
+	for n := 0; n < len(src); n++ {
+		switch {
+		case src[n] == 64:
+			break Loop
+		case src[n] != 64:
+			if src[n] == 1 {
+				break
+			}
+			result++
+		}
+	}
+
+	if result != 5 || len(src) != 8 {
+		t.Error("Error ", result)
+	}
+}
